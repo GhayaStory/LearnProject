4bb8a4a298961d12d913a384bd741b872397f8da
GhayaStory <578532261@qq.com>
2020-09-14 16:44:09 +0800

    mooi ome testDeom  allow ignore

--- LearnDemo/src/main/java/ghaya/learn/courseList/ListDemo1.java
+++ LearnDemo/src/main/java/ghaya/learn/courseList/ListDemo1.java
@@ -3,6 +3,8 @@ package ghaya.learn.courseList;
 import java.util.*;
 
 public class ListDemo1 {
+    private String[] myArray;
+
     public static void main(String[] args) {
         //Boolean、Character、Integer、Folat
         Object[] objAttr = {true,"啊",12,3,14F};//原始数组长度固定
@@ -17,6 +19,18 @@ public class ListDemo1 {
         Vector vector = new Vector();
         System.out.println(linkedList.toArray());
         List list = Collections.synchronizedList(clist);
+
+    }
+    public void setMyArray1(String [] myArray){
+        this.myArray = myArray;
+    }
+
+    public void setMyArray2(String[] newMyArray){
+        if(newMyArray == null){
+            this.myArray = new String[0];
+        }else{
+            this.myArray = Arrays.copyOf(newMyArray,newMyArray.length);
+        }
     }
 
 }
